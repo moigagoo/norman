2d9e6649bbfd90e5066f927cf0c548fa4920267a
Constantine Molchanov <moigagoo@live.com>
2020-02-06 17:30:03 +0400

    Add gen command.

--- src/norman.nim
+++ src/norman.nim
@@ -1,21 +1,26 @@
-import os, osproc, strutils
+import os, osproc, strutils, times
 
 import cligen
+
+
+const
+  migrationsPath = "migrations"
+  binPath = migrationsPath / "bin"
 
 
 proc migrate() =
   var migCount, compiledMigCount, appliedMigCount: Natural
 
-  for modulePath in walkFiles("migrations/apply_*"):
+  for modulePath in walkFiles(migrationsPath / "apply_*"):
     inc migCount
 
   echo "Compiling migrations..."
 
-  createDir "migrations" / "bin"
+  createDir binPath
 
   var cmds: seq[string]
 
-  for modulePath in walkFiles("migrations/apply_*"):
+  for modulePath in walkFiles(migrationsPath / "apply_*"):
     let (_, name, _) = splitFile modulePath
 
     cmds.add "nim c --verbosity:0 --hints:off --out:migrations/bin/$# $#" % [name, modulePath]
@@ -24,7 +29,7 @@ proc migrate() =
 
   echo "Applying migrations..."
 
-  for binName in walkFiles("migrations/bin/apply_*"):
+  for binName in walkFiles(binPath / "apply_*"):
     discard execProcess binName
     inc appliedMigCount
     echo "$#/$#: $#" % [$appliedMigCount, $migCount, binName]
@@ -32,16 +37,16 @@ proc migrate() =
 proc rollback(count: int) =
   var migCount, compiledMigCount, rollbackedMigCount: Natural
 
-  for modulePath in walkFiles("migrations/rollback_*"):
+  for modulePath in walkFiles(migrationsPath / "rollback_*"):
     inc migCount
 
   echo "Compiling migrations..."
 
-  createDir "migrations" / "bin"
+  createDir binPath
 
   var cmds: seq[string]
 
-  for modulePath in walkFiles("migrations/rollback_*"):
+  for modulePath in walkFiles(migrationsPath / "rollback_*"):
     let (_, name, _) = splitFile modulePath
 
     cmds.add "nim c --verbosity:0 --hints:off --out:migrations/bin/$# $#" % [name, modulePath]
@@ -52,7 +57,7 @@ proc rollback(count: int) =
 
   cmds.setLen 0
 
-  for binName in walkFiles("migrations/bin/rollback_*"):
+  for binName in walkFiles(binPath / "rollback_*"):
     cmds.insert(binName, 0)
 
   for cmd in cmds:
@@ -63,5 +68,19 @@ proc rollback(count: int) =
     if count > 0 and rollbackedMigCount == count:
       break
 
+proc gen(msg: string) =
+  let
+    ts = now().toTime().toUnix()
+    slug = msg.replace(" ", "_").toLowerAscii()
 
-dispatchMulti([migrate], [rollback])
+  copyFile("src/norman/models.nim", migrationsPath / "apply_$#_$#.nim" % [$ts, slug])
+
+  var lastMigration: string
+
+  for migPath in walkFiles(migrationsPath / "apply_*"):
+    lastMigration = migPath
+
+  copyFile(lastMigration, migrationsPath / "rollback_$#_$#.nim" % [$ts, slug])
+
+
+dispatchMulti([migrate], [rollback], [gen])
