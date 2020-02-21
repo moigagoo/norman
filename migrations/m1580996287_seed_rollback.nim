205fcd8c1f4d986a23aec7b2d9026d6fb332cc7a
Constantine Molchanov <moigagoo@live.com>
2020-02-21 12:16:51 +0400

    Fix rollback migration.

--- migrations/m1580996287_seed_rollback.nim
+++ migrations/m1580996287_seed_rollback.nim
@@ -18,8 +18,15 @@ dbFromEnv:
 
 withDb:
   transaction:
-    for person in mitems(getAll Person):
-      delete person
+    var pets = getAll Pet
 
-    for pet in mitems(getAll Pet):
+    for i in 0..high(pets):
+      var pet = pets[i]
       delete pet
+
+  transaction:
+    var persons = getAll Person
+
+    for i in 0..high(persons):
+      var person = persons[i]
+      delete person
