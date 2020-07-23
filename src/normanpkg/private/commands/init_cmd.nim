532372fc1a2520fa36c8d3cd9bc139b9dd67adbb
Constantine Molchanov <moigagoo@live.com>
2020-07-23 15:04:28 +0400

    Add init command.

---
+++ src/normanpkg/private/commands/init_cmd.nim
@@ -0,0 +1,35 @@
+import os
+import osproc
+import strutils
+
+import ../utils
+include "../templates/db_backend.nimf"
+include "../templates/config.nimf"
+
+
+type
+  Backend* = enum
+    sqlite, postgres
+
+proc init*(backend: Backend = sqlite) =
+  ## Scaffold folders and configs for migrations.
+
+  let appName = getAppName()
+
+  echo "Creating folders and files:"
+
+  let migDir = "migrations"
+  echo "\t$#" % migDir
+  createDir(migDir)
+
+  let cfgFile = migDir / "config.nims"
+  echo "\t$#" % cfgFile
+  cfgFile.writeFile(getConfigCode())
+
+  let modelsDir = "src" / appName / "models"
+  echo "\t$#" % modelsDir
+  createDir(modelsDir)
+
+  let dbBackendFile = "src" / appName / "db_backend.nim"
+  echo "\t$#" % dbBackendFile
+  dbBackendFile.writeFile(getDbBackendCode($backend))
--- src/normanpkg/private/utils.nim
+++ src/normanpkg/private/utils.nim
@@ -3,7 +3,15 @@ import algorithm
 
 
 proc getMigrations*: seq[string] =
+  ## List migration file in "migrations" directory, sorted A -> z.
+
   for migration in walkFiles("migrations/*.nim"):
     result.add migration
 
   sort result
+
+proc getAppName*: string =
+  ## Get the app name from the .nimble file name.
+
+  for nimbleFile in walkFiles("*.nimble"):
+    return splitFile(nimbleFile).name
